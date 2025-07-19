91b6c5e672bfe785b8d86a8040f0110eec6a46ae
Tom Harrison <tomh@apnic.net>
2025-07-19 06:29:16 +0200

    [main] Move Docker preparation process into separate script.

---
+++ docker-prep.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Must be run from a directory containing the repository content.
+
+sudo mkdir /run/sshd
+sudo chmod 0755 /run/sshd
+sudo apt-get update && sudo apt-get install gcc build-essential cpanminus cmake git libssh-dev rustc cargo stayrtr
+cargo install rtrtr
+export PATH=$PATH:/root/.cargo/bin
+
+git clone https://github.com/tanneberger/rtrlib
+cd rtrlib
+git checkout aspa-fixes
+sed -i 's/case EOD:/case EOD:\n;/' rtrlib/rtr/packets.c
+rm doxygen/examples/rtr_mgr.c
+cat doxygen/examples/CMakeLists.txt | grep -v rtr_mgr > asdf; mv asdf doxygen/examples/CMakeLists.txt
+cmake -D CMAKE_BUILD_TYPE=Debug . && make && sudo make install
+cd .. && rm -rf rtrlib
+
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64
+sudo ldconfig
+curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
+chmod +x mkcert-v*-linux-amd64
+cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+
+sudo cpanm -v --installdeps .
+export HAS_RTRCLIENT=1
+export HAS_ASPA_RTRCLIENT=1
+export HAS_RTRTR=1
+export HAS_STAYRTR=1
+export APNIC_DEBUG=1
+perl Makefile.PL && make
