a3930db6e506304e1f75755539325dcfbd96ad4c
Tom Harrison <tomh@apnic.net>
2025-07-20 11:45:59 +0200

    [main] Remove unneeded bash_profile line.

--- docker-prep.sh
+++ docker-prep.sh
@@ -4,4 +4,3 @@ sudo chmod 0755 /run/sshd
 sudo apt-get update -y && sudo apt-get install -y gcc build-essential cpanminus cmake git libssh-dev stayrtr rustup vim libio-socket-ssl-perl libnet-ip-xs-perl libjson-xs-perl openssh-server netcat-traditional
 rustup default stable
 cargo install rtrtr
-echo "export PATH=\$PATH:/root/.cargo/bin" >> /root/.bash_profile
